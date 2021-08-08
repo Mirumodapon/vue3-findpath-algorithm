3ac90583ff725d6a42745676b3d4d99ea1291897
Mirumodapon <mysteryperson520@gmail.com>
2021-08-08 15:05:49 +0800

    Init: vue3, vuex

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+	<h1>Init</h1>
+</template>
+
+<script>
+export default {
+	name: 'App',
+	components: {}
+};
+</script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+
+import store from './store';
+app.use(store);
+app.mount('#app');

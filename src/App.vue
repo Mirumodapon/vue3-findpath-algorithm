960f5fbe563defdba1bc9be3780edb94a8384e3a
Mirumodapon <mysteryperson520@gmail.com>
2021-08-09 21:51:27 +0800

    style(app,js): main style

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
-	<div v-if="mode === 'uninit'">
+	<div v-if="mode === 'uninit'" class="initform">
 		<label>Row: </label>
 		<input v-model="row" type="number" />
+		<br />
 		<label>Column: </label>
 		<input v-model="column" type="number" />
+		<br />
 		<Button @click="init">
 			Create Table
 		</Button>
 	</div>
-	<div v-if="mode !== 'uninit'">
+	<div v-if="mode !== 'uninit'" class="modeselect">
 		<Button @click="setMode('uninit')">set size</Button>
 		<Button @click="setMode('SET WALL')">set wall</Button>
 		<Button @click="setMode('SET START')">set start</Button>
 		<Button @click="setMode('SET END')">set end</Button>
-		<span>{{ mode }}</span>
 	</div>
 	<table cellspacing="0" cellpadding="0" v-if="mode !== 'uninit'">
 		<tr v-for="(row, ior) in content" :key="ior">
@@ -75,8 +76,50 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+body {
+	width: 100vw;
+	height: 100vh;
+	margin: 0;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-items: center;
+	overflow: auto;
+}
+#app {
+	padding: 20px 0;
+}
+.initform {
+	font-size: 10px;
+	width: 200px;
+	height: 200px;
+	padding: 20px;
+	border: 3px solid black;
+	border-radius: 10px;
+	* {
+		margin: 10px 0;
+	}
+	label {
+		font-size: 2em;
+	}
+	input {
+		font-size: 2.4em;
+		width: 50px;
+	}
+	button {
+		font-size: 2em;
+	}
+}
+.modeselect {
+	font-size: 10px;
+	button {
+		font-size: 2em;
+		margin: 10px 10px;
+	}
+}
 table {
 	border: 0;
+	margin: 10px auto;
 }
 </style>
